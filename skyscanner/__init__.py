041d7285b2ec5c6f323c10a4bbfd388c9d1cb216
ardydedase <ardy.dedase@gmail.com>
2015-07-23 01:55:33 +0800

    Make it work with Python 3.

--- skyscanner/__init__.py
+++ skyscanner/__init__.py
@@ -3,4 +3,4 @@ __author__ = 'Ardy Dedase'
 __email__ = 'ardy.dedase@skyscanner.net'
 __version__ = '0.1.0'
 
-from skyscanner import Flights, FlightsCache, Hotels, CarHire
+from .skyscanner import Flights, FlightsCache, Hotels, CarHire
