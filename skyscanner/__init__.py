7243f1ce24df5b3576a035b403eb9058e9826d78
ardydedase <ardy.dedase@gmail.com>
2015-08-13 10:49:02 +0800

    :zap:  Update version number

--- skyscanner/__init__.py
+++ skyscanner/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Ardy Dedase'
 __email__ = 'ardy.dedase@skyscanner.net'
-__version__ = '0.1.0'
+__version__ = '1.1.1'
 
 from .skyscanner import Flights, FlightsCache, Hotels, CarHire
